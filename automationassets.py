48fb870023ad05a6b16484c52fb73704f1deef8e
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2025-12-03 09:41:02 +0100

    Improved autonmationassets mock

--- automationassets.py
+++ automationassets.py
@@ -9,13 +9,9 @@ def set_automation_variable(key: str, value: str):
     pass
 
 def get_automation_credential(key: str) -> dict[str, str]:
-    load_dotenv(override=True)
-    if key == "CREDENTIAL_TOPDESK_API":
-        cred = {}
-        cred["username"] = get_automation_variable("TOPDESK_USERNAME")
-        cred["password"] = get_automation_variable("TOPDESK_PASSWORD")
+    if key == 'CREDENTIAL_TOPDESK_API':
+        cred = {'username': get_automation_variable('TOPDESK_USERNAME'),
+                'password': get_automation_variable('TOPDESK_PASSWORD')}
 
         return cred
-
-    return None
-
+    return {}
