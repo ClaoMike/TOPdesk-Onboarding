651a2520c914ee43ee5890f2407a72994abc86c1
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2025-12-10 09:12:47 +0100

    Prepare azure script

--- azure/azure.py
+++ azure/azure.py
@@ -1,10 +1,16 @@
-import sys
-import re
-from abc import ABC, abstractmethod
+########################################################################################################################
+
 import automationassets
+import phonenumbers
+import re
 import requests
+import sys
+from abc import ABC, abstractmethod
+from datetime import date, datetime
+from email_validator import EmailNotValidError, validate_email
+from phonenumbers import NumberParseException
 from requests.auth import HTTPBasicAuth
-from datetime import datetime
+from typing import Tuple, Union
 
 ########################################################################################################################
 
@@ -221,26 +227,16 @@ class ChangeTicket:
 
         request = self.parse_memo_text_to_dict(request)
 
-        self.fullname                   = request.get( 'Full name' )
         self.email                      = request.get( 'Email' )
         self.personal_phone_number      = request.get( 'Personal phone number (Ex. +45xxxxxxxx)' )
-        self.address                    = request.get( 'Address' )
         self.date_of_birth              = self.convert_topdesk_date_to_datetime(request.get( 'Date of birth' ))
-        self.cpr                        = request.get( 'CPR number' )
-        self.department                 = request.get( 'Department' )
-        self.employee_type              = request.get( 'Employee type' )
-        self.job_title                  = request.get( 'Job title' )
         self.start_date                 = self.convert_topdesk_date_to_datetime(request.get( 'Start date' ))
         self.end_date                   = self.convert_topdesk_date_to_datetime(request.get( 'End date' ))
         self.manager                    = request.get( 'Manager' )
-        self.hire_reason                = request.get( 'Hire reason' )
-        self.ORDER_ACCESS_KEYS          = request.get( 'Order access keys?' )
-        self.ORDER_WELCOME_PRESENT      = request.get( 'Order welcome present?' )
-        self.ec_fullname                = request.get( '(EC) Full name' )
         self.ec_email                   = request.get( '(EC) Email' )
         self.ec_personal_phone_number   = request.get( '(EC) Personal phone number (Ex. +45xxxxxxxx)' )
 
-        errors = change_validator.validate(self)
+        errors = ChangeValidator().validate(change_ticket=self)
 
         self.as_dictionary = request
         self.as_dictionary['Number'] = self.id
@@ -250,12 +246,12 @@ class ChangeTicket:
         self.as_dictionary['Template Number'] = self.template_number
 
         # reject if there are errors, approve otherwise
-        # if len(errors) > 0:
-        #     error_message = "Please re-submit the form with valid data or contact Servicedesk!\n\n" + ", ".join(errors)
-        #     ApiService().reject_change(self.id, error_message)
-        #     raise Exception(error_message)
-        # else:
-        #     ApiService().approve_change(self.id)
+        if len(errors) > 0:
+            error_message = "Please re-submit the form with valid data or contact Servicedesk!\n\n" + ", ".join(errors)
+            ApiService().reject_change(self.id, error_message)
+            raise Exception(error_message)
+        else:
+            ApiService().approve_change(self.id)
 
     def convert_topdesk_date_to_datetime(self, dt: str):
         if dt is None or dt == "":
@@ -285,11 +281,150 @@ class ChangeTicket:
         return None
 
 ########################################################################################################################
-###################### START OF SCRIPT #################################################################################
+
+class ChangeValidator():
+    def validate(self, change_ticket: ChangeTicket):
+        errors = []
+
+        # validate personal email address
+        if change_ticket.email is not None and not self.is_valid_email(change_ticket.email):
+            errors.append("Invalid email address.")
+
+        # validate emergency contact personal email address
+        if change_ticket.ec_email is not None and not self.is_valid_email(change_ticket.ec_email):
+            errors.append("Invalid emergency contact email address.")
+
+        # validate personal phone number
+        if (change_ticket.personal_phone_number is not None
+                and not self.is_valid_phone_number(change_ticket.personal_phone_number)):
+            errors.append("Invalid phone number.")
+
+        # validate emergency contact personal phone number
+        if (change_ticket.ec_personal_phone_number is not None
+                and not self.is_valid_phone_number(change_ticket.ec_personal_phone_number)):
+            errors.append("Invalid emergency contact phone number.")
+
+        # validate if the supplied manager exists and it is unique
+        if change_ticket.manager is not None:
+            found, matches = self.is_valid_manager(change_ticket.manager)
+            if not found:
+                if not matches:
+                    errors.append("Manager not found.")
+                else:
+                    matches_as_str = ", ".join(matches)
+                    errors.append(f"Found multiple matches for the manager you submitted: {matches_as_str}")
+
+        today = date.today()
+        # validate date of birth is less than today
+        if change_ticket.date_of_birth is not None and change_ticket.date_of_birth >= today:
+            errors.append("Employee's birth date cannot be in the future.")
+
+        # validate start date is at least today
+        if change_ticket.start_date is not None and change_ticket.start_date < today:
+            errors.append("Start date cannot be in the past.")
+
+        # validate end date is at least start date, if it exists at all
+        if (change_ticket.end_date is not None
+                and change_ticket.end_date is not None and change_ticket.end_date < change_ticket.start_date):
+            errors.append("End date cannot be before the start date.")
+
+        return errors
+
+    def is_valid_email(self, email: str) -> bool:
+        try:
+            validate_email(email, check_deliverability=False)  # Set to True if you want DNS checks
+            return True
+        except EmailNotValidError:
+            return False
+
+    def is_valid_phone_number(self, phone_raw: str):
+        # First try parsing with no region (only works if it starts with '+')
+        try:
+            number = phonenumbers.parse(phone_raw, None)
+            if phonenumbers.is_possible_number(number) and phonenumbers.is_valid_number(number):
+                return True
+        except NumberParseException:
+            pass
+
+        return False
+
+    def is_valid_manager(self, name: str) -> Tuple[bool, Union[dict, list, None]]:
+        results = ApiService().lookup_manager(name=name).get('results', [])
+
+        if len(results) == 1:
+            return True, results[0]
+        elif len(results) == 0:
+            return False, None
+        else:
+            return False, [r['name'] for r in results]
+
+########################################################################################################################
+
+class OnboardingSettings:
+    def __init__(self):
+        self.global_settings    = {}
+        self.widgets            = {}
+
+        self.onboarding_settings_asset = ApiService().get_asset(
+            id="625d58ac-8042-4478-93cf-a72b02336896") # Onboarding Settings Asset
+
+        self.set_global_settings()
+        self.set_widgets()
+
+    def get_activities_for_change_template(self, change_template_number):
+        country = self.global_settings.get(change_template_number)
+        widget = self.widgets.get(country)
+
+        return self.onboarding_settings_asset.get('data').get(widget)
+
+    def set_global_settings(self):
+        global_settings_entries = self.onboarding_settings_asset.get("data").get(
+            "@gridwidgetfield_961c1302-cf93-4104-9f97-3604fff406e6")
+
+        for entry in global_settings_entries:
+            global_settings_entry = ApiService().get_asset(id=entry)
+
+            country = global_settings_entry.get("data").get("country")
+            change_template = global_settings_entry.get("data").get("change-template")
+            self.global_settings[change_template] = country
+
+    def set_widgets(self):
+        widgets_as_dict = {}
+        widgets = self.onboarding_settings_asset.get("metadata").get("template").get("tabs")[0].get("columns")[1].get(
+            "widgets")
+        for widget in widgets:
+            widgets_as_dict[widget.get("title")] = widget.get("fieldId")
+
+        self.widgets = widgets_as_dict
+
+########################################################################################################################
+
+class ActivityGenerator():
+    def generate_activities_for_change(self, change_ticket: ChangeTicket):
+        templates = OnboardingSettings().get_activities_for_change_template(change_ticket.template_number)
+
+        for template in templates:
+            settings = ApiService().get_asset(id=template) #  DK entry
+
+            request_description = ""
+            fields = settings.get("data").get("fields-1").split("\n")
+            for field in fields:
+                request_description += f"{field}: {change_ticket.as_dictionary.get(field)}\n"
+
+            ApiService().create_new_activity(
+                change_name=change_ticket.id,
+                activity_template_name=settings.get("data").get("activity-template"),
+                start_date=str(date.today()),
+                end_date=str(change_ticket.start_date),
+                request_description=request_description,
+                assignee=settings.get("data").get("receiver-s-email")
+            )
+
+########################################################################################################################
+#################################################### START OF SCRIPT ###################################################
+
 parser = AzureWebhookParser()
 change_id = parser.parse_webhook()
-
-# change_id = "C 2512-0012"
 
 Configuration()
 apiService = ApiService()
@@ -300,4 +435,4 @@ change_ticket = ChangeTicket(change_ticket)
 # should no Exceptions be raised until this point, the change must be approved and ready to be processed
 ActivityGenerator().generate_activities_for_change(change_ticket)
 
-############################ END #######################################################################################
+######################################################### END ##########################################################
