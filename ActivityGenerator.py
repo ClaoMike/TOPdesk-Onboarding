e6465674a10bf534bc951ea33295e25b18974a89
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2025-12-12 12:15:57 +0100

    Add MANAGER key word

--- ActivityGenerator.py
+++ ActivityGenerator.py
@@ -15,13 +15,18 @@ class ActivityGenerator():
             for field in fields:
                 request_description += f"{field}: {change_ticket.as_dictionary.get(field)}\n"
 
+            receiver = settings.get("data").get("receiver-s-email")
+            if receiver == "MANAGER":
+                receiver_id = ApiService().lookup_manager(name=change_ticket.manager).get('results')[0].get('id')
+                receiver = ApiService().get_person_by_id(receiver_id).get("email")
+
             activity = ApiService().create_new_activity(
                 change_name=change_ticket.id,
                 activity_template_name=settings.get("data").get("activity-template"),
                 start_date=str(date.today()),
                 end_date=str(change_ticket.start_date),
                 request_description=request_description,
-                assignee=settings.get("data").get("receiver-s-email")
+                assignee=receiver
             )
 
             ApiService().solve_activity(activity.get("id"))
--- ApiService.py
+++ ApiService.py
@@ -181,3 +181,15 @@ class ApiService(Singleton):
             pass
         else:
             raise Exception(f"status: {response.status_code}, error: {response.text}")
+
+    def get_person_by_id(self, id: str):
+        url = f"{self._topdesk_url}/tas/api/persons/{id}"
+        headers = { "Content-Type": "application/json" }
+
+        response = requests.get(url, headers=headers, auth=self._authentication)
+
+        # --- Handle response ---
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"status: {response.status_code}, error: {response.text}")
